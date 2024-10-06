db4e7fdd70a786c40def01ccc5d8ae86806f5d80
Niroth36 <charitos_@hotmail.com>
2024-10-06 20:44:18 +0300

    create home page with react and fastapi for backend

---
+++ portfolio/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className='home-page'>
+            <h1>Welcome to my Portfolio</h1>
+            <p>This is the home page where I'll showcase my projects and articles.</p>
+        </div>
+    );
+};
+
+export default HomePage;
