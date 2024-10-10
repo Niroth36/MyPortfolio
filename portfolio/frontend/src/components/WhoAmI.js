53f96d7a568600c6d19473503d8d2ef332e6541a
Dimitris Charitos <it21395@hua.gr>
2024-10-10 21:08:24 +0300

    add favicon, github, linkedin icons, center alignment in text

--- portfolio/frontend/src/components/WhoAmI.js
+++ portfolio/frontend/src/components/WhoAmI.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './WhoAmI.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 const WhoAmI = () => {
     return (
         <div className='whoami-page'>
             <h1>Who Am I?</h1>
-            <p>I'm a developer passionate about building software solutions, with a strong interest in backend technologies, databases, and web development.</p>
+            <p> Motivated computer science graduate student eager for DevOps
+                engineer opportunities. Strong problem-solving skills, attention to
+                detail, and commitment to high-quality results. Skilled in program-
+                ming with a passion for learning new technologies. Ready to con-
+                tribute to challenging projects, streamline development processes,
+                and enhance overall efficiency.
+            </p>
+
+            <div className="social-links">
+                <a href="https://github.com/Niroth36" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} className="social-icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/dimitris-charitos" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+                </a>
+            </div>
         </div>
     );
 };
